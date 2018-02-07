4a17cd654f43aa73745ff493addd07ce7c6a84b2
mfreire1996 <mfreire1996@gmail.com>
2018-02-07 17:06:59 -0500

    seventh commit

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -14,7 +14,7 @@ class PlayersController < ApplicationController
 	end
 
 	def create
-		@player = Player.create(first_name: params[:player][:first_name], last_name: params[:player][:last_name], position: params[:player][:position], favorite_team: params[:player][:favorite_team], extra_equipment: params[:player][:extra_equipment], bio: params[:player][:bio])
+		@player = Player.create(user_id: params[:player][:user_id], first_name: params[:player][:first_name], last_name: params[:player][:last_name], position: params[:player][:position], favorite_team: params[:player][:favorite_team], extra_equipment: params[:player][:extra_equipment], bio: params[:player][:bio])
 		redirect_to players_path
 	end
 
